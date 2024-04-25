056f9b5e50f5c540f96619899533f3ef15b023e1
NajamulNisa <najamulnisa282@gmail.com>
2024-04-25 12:44:09 +0530

    first commit

---
+++ src/Data/PersonalData.js
@@ -0,0 +1,12 @@
+const PersonalData = {
+    firstName: "Humayun",
+    lastName: "Ashraf",
+    mobNo:"+91 6006104751",
+    email:"najamulnisa282@gmail.com",
+    address:"Srinagar",
+    aboutMe:"Hi Everyone, I am Humayun Ashraf.I learned C, C++, Python, JavaScript etc, Worked with libraries & frameworks like ReactJS, NodeJS. Have a keen knowledge of databases like MySQL, MongoDB and Firebase. Eager to contribute to innovative projects and gain hands-on experience in a dynamic and collaborative",
+    resumeLink:'https://drive.google.com/file/d/1_NnrlRNQy-ltKoBJ9Lzp1-RYeSVU35oF/view?usp=drivesdk',
+    imageUrl:''
+}
+export const autoTypeData=['MERN Stack Developer','Data Scientist', 'Open Source Contributor'];
+export default PersonalData;
---
+++ src/Data/SocialData.js
@@ -0,0 +1,16 @@
+const githubLink="https://github.com/NajamulNisa";
+const instaLink="https://www.instagram.com/najam_ul_nisa?igsh=MW1xaXQwc2twZHhtbw==/";
+const linkedInLink="https://www.linkedin.com/in/najam-ul-nisa-432b1a231?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app/";
+const twitterLink="https://x.com/najamulnisa?t=AnGbHrwVLGkwVy1SPNGCrA&s=09";
+const emailLink="mailto:najamulnisa282@gmail.com";
+
+const profileLinks={
+    githubLink,
+    instaLink,
+    linkedInLink,
+    twitterLink,
+    emailLink
+}
+const SocialData=profileLinks;
+export default SocialData;
+
